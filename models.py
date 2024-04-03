1c00d4d3f6107abcd8bc0688c96b6c45437bd96b
Hardik <hardik.goriwan@gmail.com>
2024-04-03 21:54:49 +0530

    Showing books to user done

--- models.py
+++ models.py
@@ -1,28 +1,25 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
-# from sqlalchemy.sql import func
-# from sqlalchemy import DateTime
 from werkzeug.security import generate_password_hash
-
 
 db = SQLAlchemy(app)
 
 class User(db.Model):
     user_id = db.Column(db.Integer, primary_key=True)
-    user_name = db.Column(db.String(32), unique=True, nullable=False)
+    user_name = db.Column(db.String(16), unique=True, nullable=False)
     pass_hash = db.Column(db.String(256), nullable=False)
-    name = db.Column(db.String(50))
+    name = db.Column(db.String(48), nullable=False)
     stream = db.Column(db.String(50))
-    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    is_librarian = db.Column(db.Boolean, default=False)
     books_issued = db.Column(db.Integer, nullable=False, default=0)
 
 class Section(db.Model):
     section_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(10), unique=True, nullable=False)
     description = db.Column(db.String(200))
-    date_created = db.Column(db.String, nullable=False)
+    date_created = db.Column(db.Date, nullable=False)
 
-    books = db.relationship('Book', backref='section', lazy=True) #This creates a relationship between the two tables itself using the section_id which is the foreign key in Book. Now, we don't have to give specific SQL query to select all the books from Book table where section_id in both tables is matching.
+    books = db.relationship('Book', backref='section', lazy=True, cascade='all, delete-orphan') #This creates a relationship between the two tables itself using the section_id which is the foreign key in Book. Now, we don't have to give specific SQL query to select all the books from Book table where section_id in both tables is matching.
     #backref helps in getting the name of section of the book. If we try to get section.books, we will get all the books in the section and when we try to get book.section we will get complete section object in that case too.
     #lazy just optimizes our work. It restricts the code to fetch all the books of all the sections all the time. Instead, it only works when we call it explicityly.
 
@@ -35,30 +32,27 @@ class Book(db.Model):
     image = db.Column(db.String)
     pages = db.Column(db.Integer, nullable=False)
     language = db.Column(db.String, nullable=False)
-    is_issued = db.Column(db.Boolean, nullable=False, default=False)
+    issue_num = db.Column(db.Integer, nullable=False, default=0)
 
-    # wlists = db.relationship('Waiting_list', backref='books', lazy=True)
-
-class Waitinglist(db.Model):
-    wl_id = db.Column(db.Integer, primary_key=True)
+class Requests(db.Model):
+    request_id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
     book_id = db.Column(db.Integer, db.ForeignKey('book.book_id'), nullable=False)
-    req_days = db.Column(db.Integer, default=7)
+    is_issued = db.Column(db.Boolean, nullable=True)
+    is_returned = db.Column(db.Boolean, nullable=True)
+    issue_date = db.Column(db.Date)
+    return_date = db.Column(db.Date)
 
-class Approval(db.Model):
-    approval_id = db.Column(db.Integer, primary_key=True)
-    # user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
-    # book_id = db.Column(db.Integer, db.ForeignKey('book.book_id'), nullable=False)
-    wl_id = db.Column(db.Integer, db.ForeignKey('waitinglist.wl_id'), nullable=False)
-    datetime_issued = db.Column(db.DateTime, nullable=False)
+    req_users = db.relationship('User', backref='requests', lazy=True)
+    req_books = db.relationship('Book', backref='books', lazy=True)
 
 with app.app_context():
     db.create_all()
 
-    # If admin is not there, it will create the admin
-    admin = User.query.filter_by(is_admin=True).first()
-    if not admin:
+    # If librarian is not there, it will create the librarian
+    librarian = User.query.filter_by(is_librarian=True).first()
+    if not librarian:
         password_hash = generate_password_hash('Admin')
-        admin = User(user_name='Admin', pass_hash=password_hash, name='Admin', is_admin=True)
-        db.session.add(admin)
+        librarian = User(user_name='Admin', pass_hash=password_hash, name='Admin', is_librarian=True)
+        db.session.add(librarian)
         db.session.commit()
--- routes.py
+++ routes.py
@@ -5,12 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 from datetime import datetime
 import os
-# add_category => add_section
-# edit_category => edit_section
-# delete_category => delete_section
-# show_category => show_section 
-# there is nothing like uploading the books and all there and i can make it a really better one
-# i have improvized and better standardized view of all the things out there, all are just using the tables format to store all the data and showing the id is nothing big also librarian has to remember all the id names so that he can add books there. Also librarian should also able to see all those books in those sections itself
+
 
 def authentication(func):
     @wraps(func)
@@ -19,10 +14,10 @@ def authentication(func):
             return func(*args, **kwargs)
         else:
             flash("Please Login To Continue")
-            return redirect(url_for('lib_login'))
+            return redirect(url_for('student_login'))
     return check
 
-def admin_check(func):
+def lib_check(func):
     @wraps(func)
     def check(*args, **kwargs):
         if 'user_id' not in session:
@@ -31,58 +26,26 @@ def admin_check(func):
         
         user = User.query.get(session['user_id'])
 
-        if not user.is_admin:
+        if not user.is_librarian:
             flash("You Are Not Authorized To Access This Page")
-            return redirect(url_for('sections'))
+            return redirect(url_for('lib_dashboard'))
         return func(*args, **kwargs)
     return check
+
 
 @app.route('/')
 def index():
     if 'user_id' in session:
-        return redirect(url_for('sections'))
+        return redirect(url_for('lib_dashboard'))
     else:
-        return "This will be the main page when the application is opened."        
+        return render_template('index.html')
 
 
-@app.route('/register')
-def register():
-    return render_template('register.html')
-
-@app.route('/register', methods=['POST'])
-def register_post():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    confirm_password = request.form.get('confirm-password')
-    name = request.form.get('name')
-    stream = request.form.get('stream')
-    
-    if not username or not password or not confirm_password:
-        flash("Please Fill Out The Required Fields")
-        return redirect(url_for('register'))
-    
-    if password != confirm_password:
-        flash("Password Does Not Match With Confirm Password")
-        return redirect(url_for('register'))
-    
-    user = User.query.filter_by(user_name=username).first()
-    if user:
-        flash("Username Already exists, Choose Another Username")
-        return redirect(url_for('register'))
-
-    password_hash = generate_password_hash(password)
-    new_user = User(user_name=username, pass_hash=password_hash, name=name, stream=stream)
-    db.session.add(new_user)
-    db.session.commit()
-
-    return redirect(url_for('lib_login'))
-
-
-@app.route('/lib-login')
+@app.route('/librarian-login')
 def lib_login():
     return render_template('lib_login.html')
 
-@app.route('/lib-login', methods=['POST'])
+@app.route('/librarian-login', methods=['POST'])
 def lib_login_post():
     username = request.form.get('username')
     password = request.form.get('password')
@@ -100,17 +63,23 @@ def lib_login_post():
         flash("Incorrect Password")
         return redirect(url_for('lib_login'))
 
-    if not user.is_admin:
-        flash("You Are Not A Librarian, Not Authorized To Access This Page")
-        return redirect(url_for('lib_login'))
+    if not user.is_librarian:
+        flash("You Are Not A Librarian. If You Are A Student, Login Through Student Login")
+        return redirect(url_for('student_login'))
     
     session['user_id'] = user.user_id
     flash('Login Successful')
-    return redirect(url_for('sections'))
+    return redirect(url_for('lib_dashboard'))
+
+
+@app.route('/librarian/dashboard')
+@lib_check
+def lib_dashboard():
+    return render_template('lib_dashboard.html')
 
 
 @app.route('/librarian/profile')
-@admin_check
+@lib_check
 def lib_profile():
     user = User.query.get(session['user_id'])
     # Checking if user_id is present in cookies i.e. whether user has an account or not
@@ -120,26 +89,30 @@ def lib_profile():
 @app.route('/logout')
 @authentication
 def logout():
+    user = User.query.get(session['user_id'])
     session.pop('user_id')
-    return redirect(url_for('lib_login'))
+    if user.is_librarian:
+        return redirect(url_for('lib_login'))
+    else:
+        return redirect(url_for('student_login'))
 
 
 @app.route('/librarian/sections')
-@admin_check
-def sections():
+@lib_check
+def lib_sections():
     sections = Section.query.all()
-    return render_template('sections.html', sections=sections)
+    return render_template('lib_sections.html', sections=sections)
 
 
 @app.route('/librarian/sections/add')
-@admin_check
+@lib_check
 def add_section():
     current_datetime = datetime.now()
     formatted_date = current_datetime.strftime("%Y-%m-%d")
     return render_template('section/add.html', current_date=formatted_date)
 
 @app.route('/librarian/sections/add', methods=['POST'])
-@admin_check
+@lib_check
 def add_section_post():
     title = request.form.get('title')
     description = request.form.get('description')
@@ -157,6 +130,12 @@ def add_section_post():
         flash('Description Can Contain At Most 200 Characters')
         return redirect(url_for('add_section'))
     
+    try:
+        date = datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        flash('Invalid Date')
+        return redirect(url_for('add_section'))
+    
     section = Section.query.filter_by(title=title).first()
     if section:
         flash('This Section Already Exists, Choose A Different Name')
@@ -167,24 +146,25 @@ def add_section_post():
     db.session.commit()
 
     flash('Section Added Successfully')
-    return redirect(url_for('sections'))
+    return redirect(url_for('lib_sections'))
+
 
 @app.route('/librarian/sections/edit/<int:section_id>/')
-@admin_check
+@lib_check
 def edit_section(section_id):
     section = Section.query.get(section_id)
     if not section:
         flash('Section Does Not Exist')
-        return redirect(url_for('sections'))
+        return redirect(url_for('lib_sections'))
     return render_template('section/edit.html', section=section)
 
 @app.route('/librarian/sections/edit/<int:section_id>/', methods=['POST'])
-@admin_check
+@lib_check
 def edit_section_post(section_id):
     section = Section.query.get(section_id)
     if not section:
         flash('Section Does Not Exist')
-        return redirect(url_for('sections'))
+        return redirect(url_for('lib_sections'))
     
     title = request.form.get('title')
     description = request.form.get('description')
@@ -202,8 +182,12 @@ def edit_section_post(section_id):
         flash('Description Can Contain At-Most 200 Characters')
         return redirect(url_for('edit_section', section_id=section_id))
     
-    # section_title = Section.query.filter_by(title=title).first()
-    # section_id = Section.query.get(id)
+    try:
+        date = datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        flash('Invalid Date')
+        return redirect(url_for('edit_section', section_id=section_id))
+    
     if title != section.title:
         section_title = Section.query.filter_by(title=title).first()
         if section_title:
@@ -216,58 +200,60 @@ def edit_section_post(section_id):
     db.session.commit()
 
     flash('Section Edited Successfully')
-    return redirect(url_for('sections'))
+    return redirect(url_for('lib_sections'))
+
 
 @app.route('/librarian/sections/delete/<int:section_id>/')
-@admin_check
+@lib_check
 def delete_section(section_id):
     section = Section.query.get(section_id)
     if not section:
         flash('Section Does Not Exist')
-        return redirect(url_for('sections'))
+        return redirect(url_for('lib_sections'))
     return render_template('section/delete.html', section=section)
 
 @app.route('/librarian/sections/delete/<int:section_id>/', methods=['POST'])
-@admin_check
+@lib_check
 def delete_section_post(section_id):
     section = Section.query.get(section_id)
     if not section:
         flash('Section Does Not Exist')
-        return redirect(url_for('sections'))
+        return redirect(url_for('lib_sections'))
     
     db.session.delete(section)
     db.session.commit()
 
     flash('Section Deleted Successfully')
-    return redirect(url_for('sections'))
+    return redirect(url_for('lib_sections'))
+
 
 @app.route('/librarian/sections/open/<int:section_id>/')
-@admin_check
+@lib_check
 def open_section(section_id):
     section = Section.query.get(section_id)
     if not section:
         flash('Section Does Not Exist')
-        return redirect(url_for('sections'))
+        return redirect(url_for('lib_sections'))
     books = section.books
-    return render_template('books.html', section=section, books=books)
+    return render_template('lib_books.html', section=section, books=books)
 
 
 @app.route('/librarian/sections/open/<int:section_id>/add')
-@admin_check
+@lib_check
 def add_book(section_id):
     section = Section.query.get(section_id)
     if not section:
         flash('Section Does Not Exist')
-        return redirect(url_for('sections'))
+        return redirect(url_for('lib_sections'))
     return render_template('book/add.html', section=section)
 
 @app.route('/librarian/sections/open/<int:section_id>/add', methods=['POST'])
-@admin_check
+@lib_check
 def add_book_post(section_id):
     section = Section.query.get(section_id)
     if not section:
         flash('Section Does Not Exist')
-        return redirect(url_for('sections'))
+        return redirect(url_for('lib_sections'))
     
     title = request.form.get('title')
     author = request.form.get('author')
@@ -322,13 +308,14 @@ def add_book_post(section_id):
     flash('Book Added Successfully')
     return redirect(url_for('open_section', section_id=section_id))
 
+
 @app.route('/librarian/sections/open/<int:section_id>/view/<int:book_id>')
-@admin_check
+@lib_check
 def view_book(section_id, book_id):
     section = Section.query.get(section_id)
     if not section:
         flash('Section Does Not Exist')
-        return redirect(url_for('sections'))
+        return redirect(url_for('lib_sections'))
     
     book = Book.query.get(book_id)
     if not book:
@@ -343,13 +330,14 @@ def view_book(section_id, book_id):
 
     return book
 
+
 @app.route('/librarian/sections/open/<int:section_id>/edit/<int:book_id>')
-@admin_check
+@lib_check
 def edit_book(section_id, book_id):
     section = Section.query.get(section_id)
     if not section:
         flash('Section Does Not Exist')
-        return redirect(url_for('sections'))
+        return redirect(url_for('lib_sections'))
     
     book = Book.query.get(book_id)
     if not book:
@@ -359,12 +347,12 @@ def edit_book(section_id, book_id):
     return render_template('book/edit.html', section=section, book=book)
 
 @app.route('/librarian/sections/open/<int:section_id>/edit/<int:book_id>', methods=['POST'])
-@admin_check
+@lib_check
 def edit_book_post(section_id, book_id):
     section = Section.query.get(section_id)
     if not section:
         flash('Section Does Not Exist')
-        return redirect(url_for('sections'))
+        return redirect(url_for('lib_sections'))
     
     book = Book.query.get(book_id)
     if not book:
@@ -429,13 +417,14 @@ def edit_book_post(section_id, book_id):
     flash('Book Edited Successfully')
     return redirect(url_for('open_section', section_id=section_id))
 
+
 @app.route('/librarian/sections/open/<int:section_id>/delete/<int:book_id>')
-@admin_check
+@lib_check
 def delete_book(section_id, book_id):
     section = Section.query.get(section_id)
     if not section:
         flash('Section Does Not Exist')
-        return redirect(url_for('sections'))
+        return redirect(url_for('lib_sections'))
     
     book = Book.query.get(book_id)
     if not book:
@@ -445,12 +434,12 @@ def delete_book(section_id, book_id):
     return render_template('book/delete.html', book=book)
 
 @app.route('/librarian/sections/open/<int:section_id>/delete/<int:book_id>', methods=['POST'])
-@admin_check
+@lib_check
 def delete_book_post(section_id, book_id):
     section = Section.query.get(section_id)
     if not section:
         flash('Section Does Not Exist')
-        return redirect(url_for('sections'))
+        return redirect(url_for('lib_sections'))
     
     book = Book.query.get(book_id)
     if not book:
@@ -467,3 +456,110 @@ def delete_book_post(section_id, book_id):
 
     flash('Book Removed Successfully')
     return redirect(url_for('open_section', section_id=section_id))
+
+
+
+# ----------------------------STUDENT PORTION----------------------------
+
+
+@app.route('/student-register')
+def register():
+    return render_template('student/register.html')
+
+@app.route('/student-register', methods=['POST'])
+def register_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    confirm_password = request.form.get('confirm-password')
+    name = request.form.get('name')
+    stream = request.form.get('stream')
+    
+    if not username or not password or not confirm_password:
+        flash("Please Fill Out The Required Fields")
+        return redirect(url_for('register'))
+    
+    if password != confirm_password:
+        flash("Password Does Not Match With Confirm Password")
+        return redirect(url_for('register'))
+    
+    if len(username) > 16:
+        flash("Username Can Contain At-Most 16 Characters")
+        return redirect(url_for('register'))
+    
+    if len(name) > 48:
+        flash("Name Can Contain At-Most 48 Characters")
+        return redirect(url_for('register'))
+    
+    if len(stream) > 50:
+        flash("Stream Can Contain At-Most 50 Characters")
+        return redirect(url_for('register'))
+    
+    user = User.query.filter_by(user_name=username).first()
+    if user:
+        flash("Username Already exists, Choose Another Username")
+        return redirect(url_for('register'))
+
+    password_hash = generate_password_hash(password)
+    new_user = User(user_name=username, pass_hash=password_hash, name=name, stream=stream)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('student_login'))
+
+
+@app.route('/student-login')
+def student_login():
+    return render_template('student/student_login.html')
+
+@app.route('/student-login', methods=['POST'])
+def student_login_post():
+    username = request.form.get('username')
+    password = request.form.get('password')
+
+    if not username or not password:
+        flash("Please Fill Out The Required Fields")
+        return redirect(url_for('student_login'))
+
+    user = User.query.filter_by(user_name=username).first()
+    if not user:
+        flash("Username Is Not Correct")
+        return redirect(url_for('student_login'))
+    
+    if not check_password_hash(user.pass_hash, password):
+        flash("Incorrect Password")
+        return redirect(url_for('student_login'))
+
+    if user.is_librarian:
+        flash("Please Login Through Librarian Login Page")
+        return redirect(url_for('lib_login'))
+    
+    session['user_id'] = user.user_id
+    flash('Login Successful')
+    return redirect(url_for('student_dashboard'))
+
+@app.route('/student/dashboard')
+@authentication
+def student_dashboard():
+    return render_template('student/student_dashboard.html')
+
+@app.route('/student/profile')
+@authentication
+def student_profile():
+    return render_template('student/student_profile.html')
+
+
+@app.route('/student/books')
+@authentication
+def student_books():
+    sections = Section.query.all()
+    return render_template('student/student_books.html', sections=sections)
+
+@app.route('/student/books/request/<int:book_id>')
+@authentication
+def student_request_book(book_id):
+    book = Book.query.get(book_id)
+    if not book:
+        sections = Section.query.all()
+        flash('Book Does Not Exist')
+        return render_template('student/student_books.html', sections=sections)
+    return "Coming soon"
